024333dfbcbf9c8b6a2b3faf11033226eff612dc
Ishara <isharadush94@gmail.com>
2018-06-24 22:05:43 +0530

    BankAccountDAO.java+BankAccount.java(Ishara_16211295)
    Contains the classes whic include the role of a Bankaccount
    +BankAccountDAO.java
    +BankAccount.java
    path:src\org\stock_market

---
+++ The47StockMarketSimulationGame/src/org/stock_market/dao/BankAccountDAO.java
@@ -0,0 +1,24 @@
+package org.stock_market.dao;
+
+import java.util.List;
+
+import org.stock_market.entities.BankAccount;
+public class BankAccountDAO {
+	//Ishara_Dushyanthi_16211295
+	public static List<BankAccount> getAll(){
+		List<BankAccount> bank_accounts = null;
+		return bank_accounts;
+	}
+	
+	public static BankAccount withdraw(BankAccount bank_account,double trans_amount) {
+		return bank_account;
+	}
+	
+	public static BankAccount deposit(BankAccount bank_account,double trans_amount) {
+		return bank_account;
+	}
+	
+	public static void save(BankAccount bank_account) {
+		
+	}
+}
