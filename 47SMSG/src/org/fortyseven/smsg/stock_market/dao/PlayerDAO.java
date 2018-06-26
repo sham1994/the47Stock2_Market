17b20288e8aae9e618944590f31c2992b0bcabda
Shamal De Saram <shamdzsar@gmail.com>
2018-06-26 18:07:03 +0530

    Player Functionality Update(Shamal_16211319)
    Updated Classes:
    Player

---
+++ 47SMSG/src/org/fortyseven/smsg/stock_market/dao/PlayerDAO.java
@@ -0,0 +1,40 @@
+package org.fortyseven.smsg.stock_market.dao;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.fortyseven.smsg.stock_market.entities.Player; 
+
+public class PlayerDAO {
+	private static List<Player> playerList = new ArrayList<Player>();
+	public static List<Player> getAll(){
+		
+		return playerList;
+	}
+	
+	public static Player get(String player) {
+		Player p=new Player();
+		for(Player plyr:playerList) {
+			if(plyr.getPlayer_name().equals(player)) {
+				p=plyr;
+				break;
+			}
+		}
+		return p;
+	}
+	
+	public static List<Player> save(Player player) {	        
+	      playerList.add(player);
+	      return playerList;
+	}
+	
+	public static void update(Player player) {
+		for(int i=0;i<playerList.size();i++) {
+			if(playerList.get(i).getPlayer_name().equals(player.getPlayer_name())) {
+				playerList.set(i, player);
+			}
+			
+		}
+	}
+}
