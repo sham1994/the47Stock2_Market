116cb47e3307950d6bd7772a61e995205219d3b6
Yomal_Pasindu_16211304 <yomal.devanz@gmail.com>
2018-06-26 16:28:07 +0530

    New Updates to Stock Transacactions
    Updated Stock_Transaction Classes

---
+++ 47SMSG/src/org/fortyseven/smsg/stock_market/dao/StockTransactionsDAO.java
@@ -0,0 +1,27 @@
+package org.fortyseven.smsg.stock_market.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.fortyseven.smsg.stock_market.entities.StockTransactions;
+
+public class StockTransactionsDAO {
+	private static List<StockTransactions> account_transactions=new ArrayList<StockTransactions>();
+	public static List<StockTransactions> getAll(){
+		return account_transactions;
+	}
+	
+	public static void save(StockTransactions account_transaction) {
+		account_transactions.add(account_transaction);
+	}
+	
+	public static List<StockTransactions> getTransactions(String player_name){
+		List<StockTransactions> transactions = new ArrayList<StockTransactions>();
+		for(StockTransactions transaction:account_transactions) {
+			if(transaction.getPlayer().equals(player_name)) {
+				transactions.add(transaction);
+			}
+		}
+		return transactions;
+	}
+}
